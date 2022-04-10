0e528fac1ebcad705f5a2b98ce5aa0d5342aa018
ArnaudDeza <64933703+ArnaudDeza@users.noreply.github.com>
2022-04-10 12:22:51 -0400

    Update data.py

--- Stevie_Scripts/data.py
+++ Stevie_Scripts/data.py
@@ -13,31 +13,7 @@ def load(dataset):
 
 
     nb_classes = 2
-    return (X_train, Y_train, nb_classes)
 
-
-def load1(dataset):
-    assert dataset in ['higgs', 'cd1','cd2','cd3','cd4','cd5','cd6','cd7','syn8','susy']
-    data = np.load('/u/c/dezaarna/Documents/csc413_odl_project/data/' + dataset + '.npz')
-    X_train = data['x_train']
-   
-    Y_train = data['y_train']
-
-    nb_classes = 2
-    print(X_train.shape)
-    return (X_train, Y_train, nb_classes)
-
-def foo():
-    for dataset in ['higgs', 'cd1','cd2','syn8','susy']:
-        data = np.load('/u/c/dezaarna/Documents/csc413_odl_project/data/' + dataset + '.npz')
-        X_train = data['x_train']
-    
-        Y_train = data['y_train']
-        print(dataset)
-        print(X_train.shape)
-        print(Y_train.shape)
-        print("\n \n")
-        
 if __name__ == '__main__':
     
     foo()
