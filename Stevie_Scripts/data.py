f7892652319fd3dab9dd9a9783170093c4685741
ArnaudDeza <64933703+ArnaudDeza@users.noreply.github.com>
2022-04-20 18:44:29 -0400

    Update data.py

--- Stevie_Scripts/data.py
+++ Stevie_Scripts/data.py
@@ -7,13 +7,5 @@ def load(dataset):
     assert dataset in ['higgs', 'cd1','cd2','syn8','susy']
     data = np.load('/u/c/dezaarna/Documents/csc413_odl_project/data/' + dataset + '.npz')
     X_train = data['x_train']
-   
-   
     Y_train = data['y_train']
-
-
     nb_classes = 2
-
-if __name__ == '__main__':
-    
-    foo()
